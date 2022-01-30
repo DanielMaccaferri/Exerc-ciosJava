f4d80a89e511043321ce312ae5426be21f6655a2
danielmaccaferri <danmaccaferri@hotmail.com>
2022-01-30 20:32:17 -0300

    Commit

---
+++ src/IfElseNegativoPositivo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfElseNegativoPositivo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		num = sc.nextInt();
+		if(num >= 0) {
+			System.out.println("Esse número é positivo");
+		}
+		else {
+			System.out.println("Esse número é negativo");
+		}
+		
+		sc.close();
+	}
+}
