83e224ea8e5b86f6f88814483d4f9289e7630050
danielmaccaferri <danmaccaferri@hotmail.com>
2022-02-02 08:45:27 -0300

    resto

---
+++ src/RestoDivisao.java
@@ -0,0 +1,13 @@
+
+public class RestoDivisao {
+	public static void main(String[] args) {
+		System.out.println(3 % 2);
+		System.out.println(5 % 5);
+		System.out.println(4 % 2);
+		System.out.println(4 % 1);
+		System.out.println(15 % 6);
+		System.out.println(21 % 8);
+		System.out.println(28 % 7);
+		System.out.println(45 % 14);
+	}
+}
